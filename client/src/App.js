2d07fb31dcf41948955063c816ac7795f1ddd456
Positoxin <loki.relph@gmail.com>
2023-11-07 17:03:28 -0500

    Level 1 complete

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Game from "./Components/Game";
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Game />
+    </div>
+  );
+};
+
+export default App;
